09f5ba5dbda1d0b247787afb304634f5c37a3239
Nabil Naqos <zidane.nabil07@gmail.com>
2025-08-28 12:56:20 +0200

    iki

--- Program.cs
+++ Program.cs
@@ -1,17 +1,24 @@
-﻿int p1HP = 100;
-int p2HP = 100;
-
-string p1Name = "Alex";
-string p2Name = "Calin";
-
-while (p1HP > 0 && p2HP > 0)
+﻿Fight();
+static void Fight()
 {
-    Console.WriteLine($"{p1Name}: {p1HP}");
-    Console.WriteLine($"{p2Name}: {p2HP}");
-    p2HP -= Random.Shared.Next(10, 25); // Alex slår Calin
-    p1HP -= Random.Shared.Next(10, 25); // Calin slår Alex
-    Thread.Sleep(1000);
-}
+
+
+
+    int p1HP = 100;
+    int p2HP = 100000;
+
+    string p1Name = "Alex";
+    string p2Name = "Calin";
+
+    while (p1HP > 0 && p2HP > 0)
+    {
+        Console.WriteLine($"{p1Name}: {p1HP}");
+        Console.WriteLine($"{p2Name}: {p2HP}");
+        p2HP -= Random.Shared.Next(10, 25); // Alex slår Calin
+        p1HP -= Random.Shared.Next(10, 25); // Calin slår Alex
+        Thread.Sleep(1000);
+        Console.ReadLine();
+    }
     if (p1HP > 0)
     {
         Console.WriteLine($"{p1Name} vann!");
@@ -26,7 +33,7 @@ while (p1HP > 0 && p2HP > 0)
         Console.WriteLine($"{p2Name} vann!");
     }
 
-
+}
     Console.ReadLine();
 
 
